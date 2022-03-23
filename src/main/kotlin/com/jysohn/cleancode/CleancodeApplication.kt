d382e2790cccdef22c4918cdf06ace806a6380c9
jysohn0825 <jysohn0825@naver.com>
2022-03-23 22:06:59 +0900

    init project for zerobase clean code lecture

---
+++ src/main/kotlin/com/jysohn/cleancode/CleancodeApplication.kt
@@ -0,0 +1,11 @@
+package com.jysohn.cleancode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleancodeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CleancodeApplication>(*args)
+}
